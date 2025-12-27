5631ff06f4ccde4b99370b0b277129755e29abe1
fishcpy <fishcpy@qq.com>
2025-12-27 12:58:31 +0800

    feat: 添加统计代码

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,12 @@ app.use(router);
 app.use(i18n);
 
 app.mount('#app');
+
+// 添加统计代码，仅在生产环境加载
+if (import.meta.env.PROD) {
+  const script = document.createElement('script');
+  script.defer = true;
+  script.src = 'https://um.fis.ink/script.js';
+  script.setAttribute('data-website-id', '4e7333ce-6a82-408f-9ed5-bd0d0784cb5b');
+  document.head.appendChild(script);
+}
